d7ec618f58f9bb501b340f616194f19bb0590354
PabloH <pablohenriqq01@gmail.com>
2024-02-24 11:24:52 -0300

    Adicionando pom.xml ao projeto e algumas dependencias ao projeto

--- src/com/github/playback/models/Audio.java
+++ src/com/github/playback/models/Audio.java
@@ -1,34 +1,10 @@
 package com.github.playback.models;
 
+
 public class Audio {
         private String title;
         private int totalReproduction;
         private int totalLikes;
         private double classification;
-
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public int getTotalReproduction() {
-        return totalReproduction;
-    }
-    public int reproduction(){
-        return totalReproduction++;
-    }
-
-    public int getTotalLikes() {
-        return totalLikes;
-    }
-    public int like(){
-        return totalLikes++;
-    }
-
-    public double getClassification() {
-        return classification;
-    }
 
 }
